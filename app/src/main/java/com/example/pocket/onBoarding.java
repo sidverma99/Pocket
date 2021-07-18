e1b8ac7688ce0ee01e2b7d5d5c575958427e527d
siddharth <sidverma24@gmail.com>
2021-07-18 16:33:37 +0530

    First Activity

---
+++ app/src/main/java/com/example/pocket/onBoarding.java
@@ -0,0 +1,14 @@
+package com.example.pocket;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class onBoarding extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.on_boarding_activity);
+    }
+}
